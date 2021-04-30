7f53ad735c419c23257930f73f807c8a30b2d314
Borkanie <43271944+Borkanie@users.noreply.github.com>
2021-04-30 22:19:20 +0300

    initial commit no changes to code just adding comments for better understanding

--- IronMountainTest2/Program.cs
+++ IronMountainTest2/Program.cs
@@ -12,32 +12,44 @@ namespace IronMountainTest2
 {
     class Program
     {
+        //This program contains the code for the first application in the second part of the Test
+        #region Fields
         static Settings setari ;
         static List<string> Lines = new List<string>();
         static JObject data;
+        static string ZipName="images.zip";
+        #endregion
+        //Main function we call everything from here
+        //the bot has to run once and have the App.config correctly set up
+        //it will copy all the files from the ImagePath folder into the images.zip file
+        //the name of the zipfile is hardcoded in the ZipName variable but it should be the same in both applications this and the IronMointains2.2-the second app of the second part of the test
         static void Main(string[] args)
         {
             setari = new Settings();
             setari.Separator = ConfigurationManager.AppSettings["Selector"];
             setari.ImagePath = ConfigurationManager.AppSettings["ImagePath"];
-            if (File.Exists("images.zip"))
+            //we check if the zip file already exists and in that case we complete it
+            if (File.Exists(ZipName))
             {
                 Directory.CreateDirectory(setari.Zip);
-                ZipFile.ExtractToDirectory("images.zip", Directory.GetCurrentDirectory()+"\\"+setari.Zip);
-                File.Delete("images.zip");
+                ZipFile.ExtractToDirectory(ZipName, Directory.GetCurrentDirectory()+"\\"+setari.Zip);
+                File.Delete(ZipName);
             }
             ReadFromJson();
             CreateDirectories();
             MoveAllPictures();
             WriteInMetaFile();
             
-            ZipFile.CreateFromDirectory(setari.Zip, "images.zip");
+            ZipFile.CreateFromDirectory(setari.Zip, ZipName);
             
             Directory.Delete(setari.Zip,true);
             data["Number"] = setari.Number;
             data["DayOfTheWeek"] = setari.DayOfTheWeek;
             File.WriteAllText(@"data.json", data.ToString());
         }
+        #region Methods
+        //we read the Current number and day from the data.json file
+        //please do no make the file manually it will be automaticly set up if it doesnt exist
         static void ReadFromJson()
         {
             if (File.Exists("data.json"))
@@ -65,21 +77,18 @@ namespace IronMountainTest2
                 File.WriteAllText(@"data.json", json);
             }
         }
+        //Here we write the paths and all the encodings in the Paths.meta File
         static void WriteInMetaFile()
         {
             if (!File.Exists(setari.Zip + "\\" + "Paths.meta"))
             {
                 try
                 {
-
                     var file = File.Open(setari.Zip + "\\" + "Paths.meta", FileMode.CreateNew);
                     file.Close();
                     file.Dispose();
                 }
-                catch (IOException ex)
-                {
-
-                }
+                catch (IOException) { }
             }
             
             using (StreamWriter outputFile = new StreamWriter(Directory.GetCurrentDirectory() + "\\" + setari.Zip + "\\Paths.meta",true))
@@ -91,6 +100,8 @@ namespace IronMountainTest2
                 }
             }
         }
+        //In these function we copy all the files from the target folder into our zip directory
+        //we will in the end convert the directory into a zip file by compressing
         static void MoveAllPictures()
         {
             DirectoryInfo dirInfo = new DirectoryInfo(setari.ImagePath);
@@ -119,20 +130,22 @@ namespace IronMountainTest2
               
             }
         }
+        //here we generata Directory for the current day if we have to
         static void CreateDirectories()
         {
             System.IO.Directory.CreateDirectory(setari.Zip);
             DateTime currentDate = DateTime.Now;
             System.IO.Directory.CreateDirectory(setari.Zip + "\\"+currentDate.DayOfWeek.ToString()); 
         }
+        //here we generate most of the Paths.meta line excluding the relative path to the file itself
         static string GetFirstPartOfString()
         {
-            
             string result;
             DateTime currentDate = DateTime.Now;
             JulianCalendar julianCalendar = new JulianCalendar();
             result = currentDate.Year.ToString().Substring(2)+julianCalendar.GetDayOfYear(currentDate).ToString()+ setari.Number.ToString("00000")+ setari.Separator + currentDate.ToString("yyyy/MM/dd HH:mm:ss")+ setari.Separator;
             return result;
         }
+        #endregion
     }
 }
--- IronMountainTest2/Settings.cs
+++ IronMountainTest2/Settings.cs
@@ -9,17 +9,19 @@ namespace IronMountainTest2
 
     class Settings
     {
+        //the settigns we use in our application
         public Settings()
         {
+            //starting from 0 creates errors in sql i set it up as not null in IronMointain2.2
             Number = 1;
             Zip = "zip";
         }
+        #region Properties
         public int Number { get; set; }
         public string DayOfTheWeek { get; set; }
         public string Zip { get; set; }
         public string ImagePath { get; set; }
         public string Separator { get; set; }
-
-
+        #endregion
     }
 }
